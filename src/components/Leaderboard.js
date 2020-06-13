07da75c7c3839e744e8b6c3c183c5563894cb21c
Aparna Shriraksha K.N <61300593+aparnashrirakshakn@users.noreply.github.com>
2020-06-13 11:53:27 -0500

    add leaderboard ui (#8)

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,40 @@
+import React  from 'react'
+import { connect } from 'react-redux'
+
+function Leaderboard({ users }) {
+    console.log(users)
+    return (
+        <ul>
+            {users.map((user) => (
+                <li className='user' key={user.id}>
+                    <img src={user.avatarURL} alt={`Avatar for ${user.name}`} />
+                    <div>
+                        <h1>{user.name}</h1>
+                        <p>{user.questions} Questions</p>
+                        <p>{user.answers} Answers</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+function mapStateToProps ({ users }) {
+    return {
+        users: Object.keys(users)
+            .map((id) => {
+                const { name, avatarURL, questions, answers} = users[id]
+
+                return {
+                    id,
+                    name,
+                    avatarURL,
+                    questions: questions.length,
+                    answers: answers.length
+                }
+            })
+            .sort ((a,b) => b.questions + b.answers > a.questions + a.answers)
+    }
+}
+
+export default connect(mapStateToProps)(Leaderboard)
