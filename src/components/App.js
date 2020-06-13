f5720a0e94d68c8d5c37394777d3a780f23df2d4
Aparna Shriraksha K.N <61300593+aparnashrirakshakn@users.noreply.github.com>
2020-06-13 11:49:50 -0500

    add load reducer (#7)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import Leaderboard from './Leaderboard'
 
 class App extends Component {
   componentDidMount () {
@@ -10,9 +12,10 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar />
         {this.props.loading === true
           ? null
-          : <Dashboard />}
+          : <Leaderboard />}
       </div>
     )
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import authenticatedUser from './authenticatedUser'
 import users from './users'
 import questions from './questions'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   authenticatedUser,
   users,
   questions,
+  loadingBar: loadingBarReducer
 })
