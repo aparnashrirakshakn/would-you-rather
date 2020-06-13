62fcd009930b57d7474c65022664a94aa379dfdb
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-06-13 11:49:06 -0500

    add load reducer

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
 import { setAuthenticatedUser } from '../actions/authenticatedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHENTICATED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
         dispatch(setAuthenticatedUser(AUTHENTICATED_ID))
+        dispatch(hideLoading())
       })
   }
 }
