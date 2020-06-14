7af0d9d1bfb41846b76210c717aa1b7bbf2fc0c4
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-06-14 10:55:15 -0500

    create the question ui

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getPercentage } from '../utils/helpers'
+
+const getVoteKeys = () => ['optionOneVotes', 'optionTwoVotes']
 
 class Question extends Component {
+
+  handleAnswer = (answer) => {
+    const {question, authenticatedUser} = this.props
+    this.answered = true
+
+    console.log('Add answer:', answer)
+  }
+
   render() {
+    if(this.props.question === null) {
+      return <p>This question does not exist</p>
+    }
+
+    const { question, vote, authorAvatar } = this.props
+
+    const totalVotes = getVoteKeys().reduce((total, key) => total + question[key].length, 0)
+
     return (
       <div className='poll-container'>
-        {JSON.stringify(this.props)}
+        <h1>
+          Would you rather
+        </h1>
+        <div className='poll-author'>
+          By <img arc={authorAvatar} alt="Author's Avatar" />
+        </div>
+        <ul>
+          {['optionOneText', 'optionTwoText'].map((key) => {
+            const count = question[key.slice(0,-4) + 'Votes'].length
+            console.log("VOTE: ",vote)
+            return(
+              <li key={key}
+              onClick = {() => {
+                if(vote === null && !this.answered) {
+                  this.handleAnswer(key.slice(0,-4))
+                }
+              }} 
+                className={`option ${vote === key.slice(0,-4) ? 'chosen' : ''}`}>
+                {vote === null ?
+                  question[key]
+                : <div className='result'>
+                    <span>{question[key]}</span>
+                    {<span>{getPercentage(count, totalVotes)}%</span>}
+                  </div>}
+              </li>
+            )
+            })
+          }
+        </ul>
       </div>
     )
   }
@@ -21,7 +68,7 @@ function mapStateToProps ({ authenticatedUser, questions, users}, { match }) {
     }
   }
 
-  const vote = ['optionOneVotes', 'optionTwoVotes'].reduce((vote, key) => {
+  const vote = getVoteKeys().reduce((vote, key) => {
     if (vote !== null) {
       return vote[0]
     }
