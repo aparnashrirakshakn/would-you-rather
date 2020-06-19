e17739e9f06c5d2574a5cb6fc4703947a60aa126
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-06-19 17:00:46 -0500

    refactor code

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ export default function Menu() {
                 <div>
                     <ul>
                         <li>
-                            <NavLink to='/' exact activeClassName='active'>
+                            <NavLink to='/home' exact activeClassName='active'>
                                 Home
                             </NavLink>
                         </li>
@@ -36,54 +36,6 @@ export default function Menu() {
                 </ul>
                 </div>
             </nav>
-
-            {/* <div className="card">
-                <div className="card-header">
-                    Tyler asks:
-                </div>
-                <div className="card-body">
-                    <div className="avatar-container">
-                        <img src={tyler} height="124px" width="124px" alt="author avatar" />
-                    </div>
-                    <div className="question-container">
-                        <h5 className="card-title"><b>Would you rather</b></h5>
-                        <p className="card-text">Learn React or ...</p>
-                        <button className="poll-button">View Poll</button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-header">
-                    Sarah asks:
-                </div>
-                <div className="card-body">
-                    <div className="avatar-container">
-                        <img src={sarah} height="124px" width="124px" alt="author avatar" />
-                    </div>
-                    <div className="question-container">
-                        <h5 className="card-title"><b>Would you rather</b></h5>
-                        <p className="card-text">Learn React or ...</p>
-                        <button className="poll-button">View Poll</button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-header">
-                    John asks:
-                </div>
-                <div className="card-body">
-                    <div className="avatar-container">
-                        <img src={john} height="124px" width="124px" alt="author avatar" />
-                    </div>
-                    <div className="question-container">
-                        <h5 className="card-title"><b>Would you rather</b></h5>
-                        <p className="card-text">Learn React or ...</p>
-                        <button className="poll-button">View Poll</button>
-                    </div>
-                </div>
-            </div> */}
         </div>
     )
 }
